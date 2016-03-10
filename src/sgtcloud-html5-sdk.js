ad644df94fc6d4721249a68415b8f25bf1efa04d
peisy <peisy@yoedge.com>
2016-03-10 15:02:43 +0800

    优化微信支付接口。

--- src/sgtcloud-html5-sdk.js
+++ src/sgtcloud-html5-sdk.js
@@ -7046,23 +7046,7 @@
                     playerId: playerId,
                     userId: SgtApi.context.user.userid
                 }];
-                SgtApi.doRPC(name, data, _url, function (result, order) {
-                    //微信支付
-                    wx.chooseWXPay({
-                        timestamp: order.time_start, // 支付签名时间戳，注意微信jssdk中的所有使用timestamp字段均为小写。但最新版的支付后台生成签名使用的timeStamp字段名需大写其中的S字符
-                        nonceStr: order.nonce_str, // 支付签名随机串，不长于 32 位
-                        package: 'prepay_id=' + order.prepay_id, // 统一支付接口返回的prepay_id参数值，提交格式如：prepay_id=***）
-                        signType: 'MD5', // 签名方式，默认为'SHA1'，使用新版支付需传入'MD5'
-                        paySign: order.paySign, // 支付签名
-                        success: function (res) {
-                            // 支付成功后的回调函数
-                            callback(true, order);
-                        },
-                        fail: function (res) {
-                            callback(false, res);
-                        }
-                    });
-                });
+                SgtApi.doRPC(name, data, _url, callback);
             },
 
             /**
